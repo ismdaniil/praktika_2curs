b50a1289ef9b128f9fdcc471745858befd11b61e
ismdaniil <ism-daniil@mail.ru>
2024-07-10 16:48:59 +0700

    update koordinata

--- src/koordinata.java
+++ src/koordinata.java
@@ -8,13 +8,10 @@ public class koordinata {
     }
 
     public void setKoordinataX(int koordinataX) {
-        // Проверка на корректность переданного значения координат. Оно должно быть от 0 до N.
-        // TODO: Реализовать проверку!!!
         this.koordinataX = koordinataX;
     }
 
     public void setKoordinataY(int koordinataY) {
-        // TODO: Реализовать проверку!!!
         this.koordinataY = koordinataY;
     }
 
