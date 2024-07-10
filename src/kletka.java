3203885dbf791a0e54f785af7997898ad0146b5d
ismdaniil <ism-daniil@mail.ru>
2024-07-10 16:45:01 +0700

    update Kletka

--- src/kletka.java
+++ src/kletka.java
@@ -1,4 +1,28 @@
+public class kletka extends koordinata {
+    Nasekomoe nas; // Указатель на насекомое, поле будет вызывать его методы напрямую
+    Nektar ed;     // Указатель на нектар, поле будет вызывать его методы напрямую
+    boolean flagpererisovki; // Флажок, указывающий, надо ли перерисовывать поле или нет
 
-public class kletka {
+    public kletka() {
+        super();
+        nas = null;
+        ed = null;
+        flagpererisovki = true;
+    }
 
+    public void setNasekomoe(Nasekomoe nas) {
+        this.nas = nas;
+    }
+
+    public Nasekomoe getNasekomoe() {
+        return nas;
+    }
+
+    public void setNektar(Nektar ed) {
+        this.ed = ed;
+    }
+
+    public Nektar getNektar() {
+        return ed;
+    }
 }
